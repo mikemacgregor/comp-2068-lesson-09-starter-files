470d13c9fee793684702fdc6915e3f83e7afe757
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2020-11-18 14:52:06 -0500

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+// Mongo access
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+  auth: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).catch(err => console.error(`Error: ${err}`));
+
+// Implement Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Setup our session
+
+
+// Setting up Passport
+
+
+// Setting up Passport JWT
+
+
+// register the routes
+const routes = require('./routes');
+const router = routes(express.Router(), passport);
+app.use(router);
+
+// error handling
+const { handle404s, errorHandler } = require('./errorHandling');
+app.use(handle404s);
+app.use(errorHandler);
+
+app.listen(4000, () => console.log("Always watching... on port 4000"));
